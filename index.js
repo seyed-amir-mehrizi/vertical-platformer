fe65a3de5be6643924d2f43bce1e0f7143365446
Seyed Amir Mehrizi <s.a.mehrizi1989@gmail.com>
2023-01-31 15:21:01 +0330

    player movement.

--- index.js
+++ index.js
@@ -1,51 +1,92 @@
-const canvas = document.querySelector('canvas');
-const c = canvas.getContext('2d');
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
 canvas.width = 1024;
 canvas.height = 576;
-const gravity = 0.005;
-
+const gravity = 0.5;
 
 class Player {
-    constructor(position) {
-        this.position = position;
-        this.velocity = {
-            x: 0,
-            y: 1
-        },
-            this.height = 100;
-    }
+  constructor(position) {
+    this.position = position;
+    (this.velocity = {
+      x: 0,
+      y: 1,
+    }),
+      (this.height = 100);
+  }
 
-    draw() {
-        c.fillStyle = 'red';
-        c.fillRect(this.position.x, this.position.y, 100, this.height);
-    }
+  draw() {
+    c.fillStyle = "red";
+    c.fillRect(this.position.x, this.position.y, 100, this.height);
+  }
 
-    update() {
-        this.draw();
-        this.position.y += this.velocity.y;
-        if (this.position.y + this.height + this.velocity.y < canvas.height)
-            this.velocity.y += gravity;
-        else
-            this.velocity.y = 0;
-    }
+  update() {
+    this.draw();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+    if (this.position.y + this.height + this.velocity.y < canvas.height)
+      this.velocity.y += gravity;
+    else this.velocity.y = 0;
+  }
 }
 
 let player = new Player({
-    x: 0,
-    y: 0
+  x: 0,
+  y: 0,
 });
 let player2 = new Player({
-    x: 300,
-    y: 0
-})
+  x: 300,
+  y: 0,
+});
 
-
+const keys = {
+  d: {
+    pressed: false,
+  },
+  a: {
+    pressed: false,
+  },
+};
 function animate() {
-    window.requestAnimationFrame(animate);
-    c.fillStyle = 'white';
-    c.fillRect(0, 0, canvas.width, canvas.height);
-    player.update();
-    player2.update();
+  window.requestAnimationFrame(animate);
+  c.fillStyle = "white";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  player.update();
+  player2.update();
+  player.velocity.x = 0;
+  if (keys.d.pressed) player.velocity.x = 5;
+  else if (keys.a.pressed) player.velocity.x = -5;
 }
 
 animate();
+
+// session 2  === moving element in the page
+
+window.addEventListener("keydown", (event) => {
+  switch (event.key) {
+    case "d":
+      keys.d.pressed = true;
+      break;
+    case "a":
+      keys.a.pressed = true;
+      break;
+    case "w":
+      player.velocity.y = -20;
+      break;
+
+    default:
+      break;
+  }
+});
+
+window.addEventListener("keyup", (event) => {
+  switch (event.key) {
+    case "d":
+      keys.d.pressed = false;
+      break;
+    case "a":
+      keys.a.pressed = false;
+      break;
+    default:
+      break;
+  }
+});
